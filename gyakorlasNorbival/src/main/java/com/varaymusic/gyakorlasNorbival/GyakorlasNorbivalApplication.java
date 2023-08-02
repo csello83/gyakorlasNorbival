55491a8484b912046a4ded8842067b8f41aa72d4
peter_varay <peter.varay@me.com>
2023-08-02 13:26:12 +0200

    create project backend

---
+++ gyakorlasNorbival/src/main/java/com/varaymusic/gyakorlasNorbival/GyakorlasNorbivalApplication.java
@@ -0,0 +1,13 @@
+package com.varaymusic.gyakorlasNorbival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyakorlasNorbivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyakorlasNorbivalApplication.class, args);
+	}
+
+}
---
+++ gyakorlasNorbival/src/test/java/com/varaymusic/gyakorlasNorbival/GyakorlasNorbivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varaymusic.gyakorlasNorbival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyakorlasNorbivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
